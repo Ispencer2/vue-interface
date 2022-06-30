40764688b1d79b4e95e84f2464775dbe4aebd787
Ispencer2 <ispencer@ivytech.edu>
2022-06-30 18:26:11 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="main-app" class="container">
+<h4>{{Title}}</h4>
+<font-awesome-icon icon="plus" class="mr-2"/>Add Appointment
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "MainApp",
+  data: function() {
+  return {
+  title: "Appointment List",
+  appointments: []
+    };
+  },
   components: {
-    HelloWorld
+    FontAwesomeIcon
+  },
+  mounted(){
+    axios.get("./data/appointments.json")
+    .then(response => (this.appointments = response.data));
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import"bootstrap";
+import"bootstrap/dist/css/bootstrap.css";
+
+import {library} from "fortawesome/fontawesome-svg-core";
+
+import{
+    faPlus, faMinus, faTrash, faCheck
+} from "@fortawesome/free-solid-svg-icons"
+
+library.add(faPlus, faMinus, faTrash, faCheck)
+
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
